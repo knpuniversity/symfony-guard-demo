5b3bcec33c5a8176025ee6fbedead6ecd26d6991
Ryan Weaver <ryan@thatsquality.com>
2016-05-07 19:01:58 -0400

    FormLogin: checkCredentials()

--- src/AppBundle/Security/FormLoginAuthenticator.php
+++ src/AppBundle/Security/FormLoginAuthenticator.php
@@ -6,12 +6,21 @@ use KnpU\Guard\AbstractGuardAuthenticator;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\Exception\BadCredentialsException;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\User\UserProviderInterface;
 
 class FormLoginAuthenticator extends AbstractGuardAuthenticator
 {
+    private $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
     public function getCredentials(Request $request)
     {
         if ($request->getPathInfo() != '/login_check' || !$request->isMethod('POST')) {
@@ -34,7 +43,11 @@ class FormLoginAuthenticator extends AbstractGuardAuthenticator
 
     public function checkCredentials($credentials, UserInterface $user)
     {
-        // TODO: Implement checkCredentials() method.
+        $plainPassword = $credentials['password'];
+        if (!$this->encoder->isPasswordValid($user, $plainPassword)) {
+            // throw any AuthenticationException
+            throw new BadCredentialsException();
+        }
     }
 
     public function onAuthenticationFailure(Request $request, AuthenticationException $exception)
